606e32c811e8f13a8dc49332f4a0ee5f40a8396b
SHADOW.EXE <boltnew117@gmail.com>
2025-08-31 00:19:30 +0200

    Update shadoww.lua

--- shadoww.lua
+++ shadoww.lua
@@ -136,7 +136,7 @@ Instance.new("UICorner", closeBtn).CornerRadius = UDim.new(0,8)
 
 local reopenBtn = Instance.new("TextButton", gui)
 reopenBtn.Size = UDim2.new(0,120,0,35)
-reopenBtn.Position = UDim2.new(0.5,-60,0.9,0)
+reopenBtn.Position = UDim2.new(0,20,0,20)
 reopenBtn.Text = "SHADOW"
 reopenBtn.BackgroundColor3 = Color3.fromRGB(0,0,0)
 reopenBtn.TextColor3 = Color3.fromRGB(255,0,0)
